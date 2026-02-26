ba31c01d5b1b63ce033a2da1eaa713d429a6135f
lianecagara <eckobautista03@gmail.com>
2026-02-26 15:45:58 +0800

    chore: add license and warning comments to multiple files for safety and attribution

--- zeyah-bot/components.tsx
+++ zeyah-bot/components.tsx
@@ -1,3 +1,20 @@
+/**
+ * @license MIT
+ * @author lianecagara
+ *
+ * WARNING:
+ * Modify at your own risk. You may or may not tamper with this file,
+ * but we are not responsible for any side effects, runtime failures,
+ * logic corruption, or anything that goes wrong after modification.
+ *
+ * Do not distribute repositories containing modified internal files like this one.
+ *
+ * Official repository source (if applicable):
+ * https://github.com/lianecagara/zeyah-bot
+ *
+ * If this file is not from the repository above, treat it as potentially unsafe.
+ */
+
 import Zeyah, {
   Bold,
   Break,
